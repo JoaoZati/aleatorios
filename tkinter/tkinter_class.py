c898c9e28e41c28f60f300c804bfc7e400a712bd
JoaoZati <joaozati@gmail.com>
2021-09-29 13:55:02 -0300

    exemple_multiple_window.py Fazendo testes com tkinter 2

--- tkinter/tkinter_class.py
+++ tkinter/tkinter_class.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 class Window(tk.Tk):
@@ -10,12 +11,18 @@ class Window(tk.Tk):
         self.columnconfigure(0, weight=1)
 
         self.create_label()
+        self.button = self.create_button()
 
     def create_label(self):
         label_font = ('times', 40, 'bold')
         label = tk.Label(self, text='Text Label')
         label.config(font=label_font)
         label.grid(column=0, row=0)
+
+    def create_button(self):
+        button = ttk.Button(self, text = 'Click ME')
+        button.grid(column=0, row=1)
+        return button
 
 window = Window()
 window.mainloop()
